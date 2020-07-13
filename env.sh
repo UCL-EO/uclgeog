2ae57b568d316b9e3a843f80cf6ab16510a95ded
Professor Philip Lewis <dr.p.e.lewis@gmail.com>
2020-07-13 22:18:55 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,10 @@
-# environment variables fdor setup
+#!/bin/sh
+#
+# Use this file to personalise your setup
+# 
+# environment variables for consistent setup
+# this is easier than yaml or anything else f
+# or simple 1-deep info
 export SETUP_URL=https://github.com/profLewis/uclgeog
 export SETUP_VERSION=1.0.11
 export SETUP_META=meta.yaml
