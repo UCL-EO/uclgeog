6cd986a84ba6dba490a6ccc536c2aa3e6538a987
Professor Philip Lewis <dr.p.e.lewis@gmail.com>
2020-07-13 22:05:22 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,3 +3,6 @@ export SETUP_URL=https://github.com/profLewis/uclgeog
 export SETUP_VERSION=1.0.11
 export SETUP_META=meta.yaml
 export SETUP_NAME=uclgeog
+export SETUP_DESCRIPTION='UCL Geography course notes'
+export SETUP_AUTHOR='Prof. P. Lewis'
+export SETUP_AUTHOR_EMAIL='p.lewis@ucl.ac.uk'
